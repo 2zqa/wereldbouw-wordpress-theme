654cf640f7db7fc1a1a1f0aa9123f0bdebb85946
2zqa <25235249+2zqa@users.noreply.github.com>
2021-05-09 15:24:23 +0200

    Update header
    Co-Authored-By: Rutger Kok <rutger.kok@live.com>

--- header.php
+++ header.php
@@ -25,41 +25,44 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'wereldbouw' ); ?></a>
 
-	<header id="masthead" class="site-header" style="background-image: url(<?= header_image(); ?>);">
+	<?php if (is_front_page()): ?>
+	<header id="masthead" class="site-header site-header-image" style="background-image: url(<?= htmlspecialchars(header_image()) ?>);">
+	<?php else: ?>
+	<header id="masthead" class="site-header">
+	<?php endif; ?>
 		<div class="site-header-content">
 			<div class="site-branding">
-				<?php
-				the_custom_logo();
-				if ( is_front_page() && is_home() ) :
-					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php
-				else :
-					?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php
-				endif; ?>
+				<?php the_custom_logo()	?>
 			</div><!-- .site-branding -->
 
-			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wereldbouw' ); ?></button>
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-					)
-				);
-				?>
-			</nav><!-- #site-navigation -->
+			<div class="site-header-inner">
+				<div class="site-titles">
+					<?php if (is_front_page()):	?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php else:	?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php endif ?>
 
-			<div class="site-subtitle">
-				<?php
-				$wereldbouw_description = get_bloginfo( 'description', 'display' );
-				if ( $wereldbouw_description || is_customize_preview() ) :
+					<?php
+					$wereldbouw_description = get_bloginfo( 'description', 'display' );
+					if ( $wereldbouw_description || is_customize_preview() ) :
+						?>
+						<p class="site-description"><?php echo $wereldbouw_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+					<?php endif; ?>
+				</div> <!-- .site-subtitle -->
+				<nav id="site-navigation" class="main-navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wereldbouw' ); ?></button>
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+						)
+					);
 					?>
-					<p class="site-description"><?php echo $wereldbouw_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-				<?php endif; ?>
-			</div>
+				</nav><!-- #site-navigation -->
+			</div> <!-- .site-header-inner -->
+
+			
 		</div><!-- .site-header-content -->
 	</header><!-- #masthead -->
