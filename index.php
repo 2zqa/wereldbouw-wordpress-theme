895e4fca6b72768c9eae92c606ab62fa38523878
Rutger Kok <rutger.kok@live.com>
2021-05-23 10:40:51 +0200

    Use grid layout for news items

--- index.php
+++ index.php
@@ -28,19 +28,26 @@ get_header();
 				<?php
 			endif;
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			?>
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+			<div class="post-list">
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
 
-			endwhile;
+					/*
+					* Include the Post-Type-specific template for the content.
+					* If you want to override this in a child theme, then include a file
+					* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+					*/
+					get_template_part( 'template-parts/content', get_post_type() );
 
+				endwhile;
+				?>
+			</div> <!-- .post-list -->
+
+			<?php
 			the_posts_navigation();
 
 		else :
