78c3d9f205a84ff3583976717e1f5e943e00bd37
Rutger Kok <rutger.kok@live.com>
2026-03-23 22:04:14 +0100

    Links and menu items

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width">
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+ <?php wp_body_open(); ?>
+ <nav id="menu" role="navigation" aria-label="<?php esc_attr_e( 'Primary Navigation', 'blankslate' ); ?>" itemscope itemtype="https://schema.org/SiteNavigationElement">
+  <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
+  <div id="search" role="search" aria-label="<?php esc_attr_e( 'Search', 'blankslate' ); ?>"><?php get_search_form(); ?></div>
+ </nav>
+ <div id="wrapper" class="hfeed">
+  <header id="header" role="banner">
+
+   <div id="<?= is_front_page()? "branding-home" : "branding" ?>">
+    <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+     <?php
+      if ( is_front_page()) { echo '<h1>'; }
+      echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
+      if ( is_front_page()) { echo '</h1>'; }
+     ?>
+    </div>
+    <div id="site-description"<?php if ( !is_single() ) { echo ' itemprop="description"'; } ?>><?php bloginfo( 'description' ); ?></div>
+   </div>
+  </header>
+  <div id="container">
+  <main id="content" role="main">
